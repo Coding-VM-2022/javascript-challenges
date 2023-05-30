a6bf66d0280b0836dd275a99d2975ab6c35d8972
Tejas-Naik <RNTejas2005@gmail.com>
2023-05-30 01:07:26 -0700

    Completed Challege #5

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Part 1
 /*
 Coding Challenge #1
 Mark and John are trying to compare their BMI (Body Mass Index), which is
@@ -28,7 +29,7 @@ const caclBMI = function (mass, height) {
 const higherBMI = function (marksHeight, marksWeight, johnsHeight, johnsWeight) {
     const marksBMI = caclBMI(marksWeight, marksHeight);
     const johnsBMI = caclBMI(johnsWeight, johnsHeight);
-    
+
     return marksBMI > johnsBMI;
 }
 
@@ -50,7 +51,7 @@ BMI (28.3) is higher than John's (23.9)!"
 
 Hint: Use an if/else statement �
 
-GOOD LUCK � 
+GOOD LUCK �
 
 const caclBMI = function (mass, height) {
     return mass / (height ** 2);
@@ -144,9 +145,52 @@ Test data:
 
 GOOD LUCK �
 
-*/
-
 const bill = Number(prompt("Whats the bill?"));
 const tip = (bill >= 50 && bill <= 300) ? bill * .15 : bill * .2
 const total = bill + tip;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`);
+
+*/
+
+// Part 2
+/*
+Coding Challenge #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so
+one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+
+2. Use the function to calculate the average for both teams
+
+3. Create a function 'checkWinner' that takes the average score of each team
+as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+Data 2
+
+5. Ignore draws this time
+
+Test data:
+§ Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+§ Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+*/
+
+const calcAvarage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const dolphinsAvarage = calcAvarage(85, 54, 41);
+const koalasAvarage = calcAvarage(23, 34, 27);
+
+const checkWinner = function (dolphinsAvarage, koalasAvarage) {
+    if (dolphinsAvarage >= (koalasAvarage * 2)) console.log(`Dolphins win (${dolphinsAvarage} vs ${koalasAvarage})`);
+    else if (koalasAvarage >= (dolphinsAvarage * 2)) console.log(`Koalas win (${koalasAvarage} vs ${dolphinsAvarage})`);
+    else console.log("Nobody Wins");
+}
+
+checkWinner(dolphinsAvarage, koalasAvarage);
