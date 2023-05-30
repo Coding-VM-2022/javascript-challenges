537c73c5a970824d67c0ec70961f7e0eb6d598d5
Tejas-Naik <RNTejas2005@gmail.com>
2023-05-30 00:33:39 -0700

    Completed Challenge #2

--- script.js
+++ script.js
@@ -21,7 +21,39 @@ m tall.
 m tall.
 
 GOOD LUCK �
+const caclBMI = function (mass, height) {
+    return mass / (height ** 2);
+}
+
+const higherBMI = function (marksHeight, marksWeight, johnsHeight, johnsWeight) {
+    const marksBMI = caclBMI(marksWeight, marksHeight);
+    const johnsBMI = caclBMI(johnsWeight, johnsHeight);
+    
+    return marksBMI > johnsBMI;
+}
+
+const markHigherBMI = higherBMI(1.69, 78, 1.95, 92);
+// const markHigherBMI = higherBMI(1.88, 95, 1.76, 85);
+console.log(markHigherBMI);
+
+
+Coding Challenge #2
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+
+Hint: Use an if/else statement �
+
+GOOD LUCK � 
+
 */
+
 const caclBMI = function (mass, height) {
     return mass / (height ** 2);
 }
@@ -30,10 +62,12 @@ const higherBMI = function (marksHeight, marksWeight, johnsHeight, johnsWeight) 
     const marksBMI = caclBMI(marksWeight, marksHeight);
     const johnsBMI = caclBMI(johnsWeight, johnsHeight);
 
-    return marksBMI > johnsBMI;
+    if (marksBMI > johnsBMI) {
+        console.log(`Marks BMI(${marksBMI}) is higher than John's BMI(${johnsBMI})`);
+    } else if (johnsBMI > marksBMI) {
+        console.log(`Johns BMI(${johnsBMI}) is higher than Marks's BMI(${marksBMI})`);
+    }
 }
 
-const markHigherBMI = higherBMI(1.69, 78, 1.95, 92);
-// const markHigherBMI = higherBMI(1.88, 95, 1.76, 85);
-console.log(markHigherBMI);
-
+// const markHigherBMI = higherBMI(1.69, 78, 1.95, 92);
+const markHigherBMI = higherBMI(1.88, 95, 1.76, 85);
