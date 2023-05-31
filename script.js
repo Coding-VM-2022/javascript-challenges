2c06fafb70551ca052b206d13e8577be7c70cb27
Tejas-Naik <RNTejas2005@gmail.com>
2023-05-30 23:46:02 -0700

    Completd Challenge #13

--- script.js
+++ script.js
@@ -673,7 +673,6 @@ Test data for bonus:
 Hints: Use many of the tools you learned about in this and the last section �
 
 GOOD LUCK �
-*/
 
 const poll = {
     question: "What is your favourite programming language?",
@@ -693,3 +692,31 @@ document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer
 
 poll.showResults.call({ answers: [5, 3, 2] });
 poll.showResults.call({ answers: [5, 3, 2, 9, 9] });
+
+
+Coding Challenge #2
+This is more of a thinking challenge than a coding challenge �
+Your tasks:
+
+1. Take the IIFE below and at the end of the function, attach an event listener that
+changes the color of the selected h1 element ('header') to blue, each time
+the body element is clicked. Do not select the h1 element again!
+
+2. And now explain to yourself (or someone around you) why this worked! Take all
+the time you need. Think about when exactly the callback function is executed,
+and what that means for the variables involved in this example.
+    (function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+    })();
+GOOD LUCK �
+
+*/
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+    document.body.addEventListener('click', function () {
+        header.style.color = "blue";
+    })
+})();
