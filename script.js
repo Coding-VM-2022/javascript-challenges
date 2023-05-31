41717ab65b9989cac63087a1e35d5d646fe9f39d
Tejas-Naik <RNTejas2005@gmail.com>
2023-05-30 22:32:10 -0700

    Completed Challenge #11

--- script.js
+++ script.js
@@ -539,7 +539,6 @@ each event happened (a football game has 90 minutes plus some extra time).
 Your tasks:
 
 GOOD LUCK �
-*/
 
 const gameEvents = new Map([
     [17, '⚽ GOAL'],
@@ -579,4 +578,50 @@ for (const [time, event] of gameEvents) {
     console.log(`[${half}HALF] ${time} : ${event}`);
 }
 
+Coding Challenge #4
+Write a program that receives a list of variable names written in underscore_case
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to
+insert the elements), and conversion will happen when the button is pressed.
 
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable
+ calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+
+Hints:
+§ Remember which character defines a new line in the textarea �
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working �
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK �
+
+*/
+document.body.append(document.createElement('textarea'));
+const button = document.createElement('button');
+button.textContent = 'Click Me';
+document.body.appendChild(button);
+
+document.querySelector('button').addEventListener('click', function () {
+    const text = document.querySelector('textarea').value;
+    const rows = text.split("\n");
+    console.log(rows);
+
+    for (const row of rows) {
+        const [first, second] = row.toLowerCase().trim().split("_");
+        const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+        console.log(`${output.padEnd(25, " ")}${"✅".repeat(rows.indexOf(row) + 1)}`);
+    }
+})
