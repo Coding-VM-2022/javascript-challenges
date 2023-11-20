355dc9a6a3b35777600fb8d5aea83115e5fafa13
Tejas Naik <74360622+Tejas-Naik@users.noreply.github.com>
2023-11-20 16:28:34 +0000

    Completed Challenge @2

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 // Challenge #1
 const johnHeight = 1.95;
 const markHeight = 1.69;
@@ -17,5 +18,18 @@ if (markBMI > johnBMI){
 }
 
 console.log(markHigherBMI);
-
+*/
 // Challenge #2
+const johnHeight = 1.95;
+const markHeight = 1.69;
+const johnMass = 92;
+const markMass = 78;
+
+const johnBMI = +(johnMass / (johnHeight**2)).toFixed(2);
+const markBMI = +(markMass / (markHeight**2)).toFixed(2);
+
+if (markBMI > johnBMI){
+    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`)
+} else if (markBMI < johnBMI){
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!"`)
+}
