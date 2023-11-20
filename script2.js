987421eec1bf2b717336a19c0250625fc54c81f8
Tejas Naik <74360622+Tejas-Naik@users.noreply.github.com>
2023-11-20 16:38:24 +0000

    Completed challenge #3

--- script2.js
+++ script2.js
@@ -18,7 +18,6 @@ if (markBMI > johnBMI){
 }
 
 console.log(markHigherBMI);
-*/
 // Challenge #2
 const johnHeight = 1.95;
 const markHeight = 1.69;
@@ -32,4 +31,19 @@ if (markBMI > johnBMI){
     console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`)
 } else if (markBMI < johnBMI){
     console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!"`)
+}
+*/
+
+// Challenge #3
+const dolphinsAvarage = (97+ 112 + 101) / 3;
+const koalasAvarage = (109+ 95 + 106) / 3;
+
+if (dolphinsAvarage >= 100 && dolphinsAvarage > koalasAvarage){
+    console.log("Dolphins win");
+} else if (koalasAvarage >= 100 && koalasAvarage > dolphinsAvarage) {
+    console.log("Koalas win");
+} else if ((koalasAvarage>=100) === (dolphinsAvarage>=100)) {
+    console.log("Draw")
+} else {
+    console.log("Nobody wins")
 }
