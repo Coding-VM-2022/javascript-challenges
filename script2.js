39d8af809d5f14a3bb39dd5197a1e8b1f3d5cee1
Tejas Naik <74360622+Tejas-Naik@users.noreply.github.com>
2023-12-11 16:26:32 +0000

    Test commit

--- script2.js
+++ script2.js
@@ -115,7 +115,6 @@ if (mark.BMI > john.BMI) {
 } else {
     console.log(`John's BMI(${john.BMI}) is higher than Mark's BMI(${mark.BMI})`);
 }
-*/
 // Challenge #8
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
@@ -141,3 +140,67 @@ console.log(bills);
 console.log(tips);
 console.log(totals);
 console.log(calcAvarage(bills));
+*/
+// Challenge #9
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+        'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// Create one player array for each team(variables 'players1' and
+// 'players2')
+
+const players1 = game.players[0];
+const players2 = game.players[1];
+console.log(players1, players2);
+
+const [gk, ...otherPlayers] = players1;
+console.log(gk, otherPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+
